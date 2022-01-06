7f12cecbf022d1fe04b872f99e01a4e77c71c37b
Zander Roi Tabelona <92770702+zndrroi@users.noreply.github.com>
2022-01-06 15:13:10 +0800

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Home_model','Department_model');
+$autoload['model'] = array('Home_model','Department_model','Staff_model');
